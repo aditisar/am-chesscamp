c17f640084d6509ee6f81f1a2d836c18591be366
Aditi Sarkar <aditi@cmu.edu>
2014-04-24 08:15:02 -0400

    started modifying style to my phase 3 style

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,29 @@
+class LocationsController < ApplicationController
+  def index
+    @active_locations = Location.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+    @inactive_curriculums = Location.inactive.alphabetical.paginate(:page => params[:page]).per_page(10)
+  end
+
+  def show
+    @locations = @curriculum.camps.chronological.to_a
+  end
+
+  def new
+    @location = Location.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+
+end
