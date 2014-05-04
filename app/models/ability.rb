330011d73d62cb18ba92f726eb70dd315c8e1b72
Aditi Sarkar <aditi@cmu.edu>
2014-05-04 10:28:48 -0400

    abilities modified for instructor to only see his students

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,10 +9,29 @@ class Ability
         can :manage, :all
       
       elsif user.role? :instructor
+
+        #can see a list of instructors
+        can :index, Instructor
+
+        can :read, Student do |this_student|  
+          instructor_students = user.instructor.camps.map{|c| c.students.map(&:id)}.flatten
+          instructor_students.include? this_student.id 
+        end
+
+        #can update their own information
         can :update, Instructor do |instructor|  
           instructor.id == user.instructor_id
         end
-        can :read, :all
+
+        #can read their own instructor page
+        can :read, Instructor do |instructor|
+          instructor.id == user.instructor_id
+        end
+
+        can :index, Camp
+
+        can :read, Camp
+
 
       else
         can :read, :all
